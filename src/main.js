e6b08988f0ee0127c613f48d5f4177fc22eaa99c
Daniil Grachev <daniilgracev765@yandex.ru>
2025-07-16 13:49:35 +0300

    коммит 5

--- src/main.js
+++ src/main.js
@@ -107,6 +107,7 @@ function analyzeSalesData(data, options) {
       // Посчитать прибыль: выручка минус себестоимость
       const profit = revenue - cost;
       // Увеличить общую накопленную прибыль (profit) у продавца
+      seller.profit += profit;
 
       // Учёт количества проданных товаров
       if (!seller.products_sold[item.sku]) {
